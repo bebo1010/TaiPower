a2e4aa20a54190c55cdad3572f419dc4f5d1ab5e
張議隆 <nick900828@gmail.com>
2023-05-05 08:31:33 +0000

    Un-debugged server side script

---
+++ website/server_side_script.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
+    echo "日期：", $_POST["data"]["time"];
+    echo " 地點：", $_POST["data"]["location"];
+    echo " 度數：", $_POST["data"]["power"];
+    ?>
+    </body>
+</html>
+
+
